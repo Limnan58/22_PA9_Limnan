fa9eef9164ea7aef145c713f8b5dd3b134a6a672
blasterscx <41160786+blasterscx@users.noreply.github.com>
2020-06-15 17:44:14 +0800

    Created PA9

---
+++ PA9_Limnan/PA9_Limnan/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PA9_Limnan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.amount = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.selectcurrency = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // amount
+            // 
+            this.amount.AutoSize = true;
+            this.amount.Location = new System.Drawing.Point(102, 101);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(56, 17);
+            this.amount.TabIndex = 0;
+            this.amount.Text = "Amount";
+            this.amount.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(250, 96);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(236, 22);
+            this.tb1.TabIndex = 1;
+            // 
+            // selectcurrency
+            // 
+            this.selectcurrency.AutoSize = true;
+            this.selectcurrency.Location = new System.Drawing.Point(102, 190);
+            this.selectcurrency.Name = "selectcurrency";
+            this.selectcurrency.Size = new System.Drawing.Size(108, 17);
+            this.selectcurrency.TabIndex = 2;
+            this.selectcurrency.Text = "Select Currency";
+            this.selectcurrency.Click += new System.EventHandler(this.selectcurrency_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(250, 188);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 21);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "USD";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(250, 234);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(120, 21);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Japanese Yen";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Converted Amount";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(250, 308);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(236, 22);
+            this.tb2.TabIndex = 6;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(634, 307);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 8;
+            this.btn1.Text = "Convert";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(634, 369);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 9;
+            this.btn2.Text = "Clear";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.selectcurrency);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.amount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Label amount;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label selectcurrency;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ PA9_Limnan/PA9_Limnan/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA9_Limnan
+{
+    public partial class Form1 : Form
+    {
+        public float currency;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectcurrency_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                currency = float.Parse(tb1.Text);
+                if (radioButton1.Checked)
+                {
+                    tb2.Text = (currency * 0.74f).ToString();
+                }
+                else if (radioButton2.Checked)
+                {
+                    tb2.Text = (currency * 81.97f).ToString();
+                }
+                if (currency < 0)
+                {
+                    tb2.Text = ("Please enter a positive number.");
+                }
+            }
+            catch
+            {
+                tb2.Text = ("Not a valid number");
+            }
+           
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            tb1.Clear();
+            tb2.Clear();
+
+        }
+    }
+}
